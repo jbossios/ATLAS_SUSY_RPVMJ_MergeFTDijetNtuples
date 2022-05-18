4c204da64f6c15cee363c65c5d8bc5026b5c3809
Anthony Badea <anthony.badea@cern.ch>
2022-05-18 13:09:21 +0200

    working output directory infering

--- add_normweights.py
+++ add_normweights.py
@@ -8,6 +8,7 @@ from ROOT import *
 import logging
 
 # create log
+logging.basicConfig(level = 'INFO', format = '%(levelname)s: %(message)s')
 log = logging.getLogger()
 
 def add_normweight(conf):
@@ -22,18 +23,21 @@ def add_normweight(conf):
   sum_of_weights = conf["sum_of_weights"]
   debug = conf["debug"]
 
-  full_file_name = '{}/{}'.format(input_dir, file_name)
-  log.info('Processing {}'.format(full_file_name))
+  # check if should infer output directory
+  if not output_dir:
+    output_dir = os.path.dirname(file_name)
+
+  log.info('Processing {}'.format(file_name))
 
   # Make sure input file is a ROOT file
   if not file_name.endswith('.root'):
     pass
   
   # Open ROOT file and get TTree
-  tfile = TFile.Open(full_file_name)
+  tfile = TFile.Open(file_name)
   # Identify DSID for this file
-  dsid = int(input_dir.split('user.')[1].split('.')[2])
-  log.debug('dsid: {} identified from {}'.format(dsid, input_dir))
+  dsid = int(file_name.split('user.')[1].split('.')[2])
+  log.debug('dsid: {} identified from {}'.format(dsid, file_name))
   # Get TTree and add it to the TChain
   ttree = tfile.Get(ttree_name)
   if not ttree:
@@ -85,7 +89,7 @@ def add_normweight(conf):
 
     # Write new DFs as TTrees to ROOT files (unless requested not to)
     if not dry_run:
-      out_full_file_name = '{}/{}'.format(output_dir, file_name.replace('.root', '_expanded.root'))
+      out_full_file_name = '{}/{}'.format(output_dir, os.path.basename(file_name).replace(".root", "_expanded.root"))
       log.info('Taking a snapshot of the dataframe as a ROOT file: {}'.format(out_full_file_name))
       my_df_dsid.Snapshot(ttree_name, out_full_file_name);
 
@@ -120,7 +124,9 @@ def expand_ttrees(args):
 
   # get list of files
   file_list = handleInput(input_dir)
-  
+  # remove already expanded files
+  file_list = [i for i in file_list if '_expanded.root' not in i]
+
   # Create logger
   logging.basicConfig(level = 'INFO' if not debug else 'DEBUG', format = '%(levelname)s: %(message)s')
   log = logging.getLogger()
@@ -133,14 +139,14 @@ def expand_ttrees(args):
     # Make sure it's a ROOT file
     if not file_name.endswith('.root'): continue
     # Get metadata from all files, even those w/ empty TTrees
-    full_file_name = '{}/{}'.format(input_dir, file_name)
+    # full_file_name = '{}/{}'.format(input_dir, file_name)
     try:
-      tfile = TFile.Open(full_file_name)
+      tfile = TFile.Open(file_name)
     except OSError:
-      raise OSError('{} can not be opened'.format(full_file_name))
+      raise OSError('{} can not be opened'.format(file_name))
     # Identify DSID for this file
-    dsid = int(input_dir.split('user.')[1].split('.')[2])
-    log.debug('dsid: {} identified from input_dir: {}'.format(dsid, input_dir))
+    dsid = int(file_name.split('user.')[1].split('.')[2])
+    log.debug('dsid: {} identified from input_dir: {}'.format(dsid, file_name))
     # Get sum of weights from metadata
     metadata_hist = tfile.Get('MetaData_EventCount')
     if dsid not in sum_of_weights:
@@ -164,21 +170,21 @@ def expand_ttrees(args):
       #"log" : log, 
       "debug" : debug
     })
-  print(config[0])
 
   # Add normweight and write a new ROOT file
-  if ncpu > 1:
-    # launch pool
-    results = Pool(ncpu).map(add_normweight, config)
-  else:
-    for conf in config:
-      add_normweight(**conf)
+  if not dry_run:
+    if ncpu > 1:
+      # launch pool
+      results = Pool(ncpu).map(add_normweight, config)
+    else:
+      for conf in config:
+        add_normweight(conf)
   
   log.info('>>> All done <<<')
 
 def handleInput(data):
     # otherwise return 
-    elif os.path.isfile(data) and ".root" in os.path.basename(data):
+    if os.path.isfile(data) and ".root" in os.path.basename(data):
         return [data]
     elif os.path.isfile(data) and ".txt" in os.path.basename(data):
         return sorted([line.strip() for line in open(data,"r")])
@@ -200,15 +206,16 @@ if __name__ == '__main__':
   parser.add_argument('--debug', action='store_true', dest='debug', default=False, help='Debug flag')
   parser.add_argument("--ncpu", help="Number of cores to use for multiprocessing. If not provided multiprocessing not done.", default=1, type=int)
   args = parser.parse_args()
-
+  print(args.output_dir)
   # Protections
   if not args.input_dir:
     print('ERROR: please specify input directory, exiting')
     parser.print_help()
     sys.exit(1)
   if not args.output_dir:
-    print('ERROR: please specify output directory, exiting')
-    parser.print_help()
-    sys.exit(1)
+    #print('ERROR: please specify output directory, exiting')
+    log.info("No output directory provided so inferring from filenames")
+    #parser.print_help()
+    #sys.exit(1)
   
   expand_ttrees(args)
